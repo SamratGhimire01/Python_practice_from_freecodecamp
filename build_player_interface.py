7e2825ac399dfffac1a64566ddb7ec83200667fd
SamratGhimire01 <samratghimire01@gmail.com>
2026-03-01 12:55:34 +0545

    build_player_inerface..

---
+++ build_player_interface.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+import random
+
+class Player(ABC):
+    def __init__(self):
+        self.moves = []
+        self.position = (0, 0)
+        self.path = [self.position]
+
+    def make_move(self):
+       
+        move = random.choice(self.moves)
+        
+        
+        x = self.position[0] + move[0]
+        y = self.position[1] + move[1]
+        self.position = (x, y)
+        
+        
+        self.path.append(self.position)
+        
+        
+        return self.position
+
+    @abstractmethod
+    def level_up(self):
+        pass
+
+class Pawn(Player):
+    def __init__(self):
+
+        super().__init__()
+
+        self.moves = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+    
+    def level_up(self):
+
+        diagonals = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+        self.moves.extend(diagonals)
