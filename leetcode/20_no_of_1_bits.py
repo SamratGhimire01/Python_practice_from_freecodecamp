132bfc7b4263ed28b242e68e6c22c211cb7d5226
SamratGhimire01 <samratghimire01@gmail.com>
2026-03-13 23:18:39 +0545

    updating all the file from main to master branch

---
+++ leetcode/20_no_of_1_bits.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        count = 0
+        while n>0:
+            n &= (n-1)
+            count +=1
+        return count
+
+s= Solution()
+print(s.hammingWeight(2147483645))
+        
